a1a211d88decf1248aa6fa00b9c5efa741a53c6d
Karen Stewart <karenstewart1117@gmail.com>
2022-02-18 10:27:10 +0100

    create planets controller file

---
+++ server/src/routes/planets/planets.controller.js
@@ -0,0 +1,15 @@
+// controllers and routers are 1:1 - each controller has a router, vice versa
+// import the data from model folder
+const { getAllPlanets } = require("../../models/planets.model");
+
+function httpGetAllPlanets(req, res) {
+  // returns response with http status 200 and the planets array as json
+  // using return makes sure the function stops executing, and it makes the code more readable; but lot of code wont include it.
+  return res.status(200).json(getAllPlanets());
+}
+
+// exports getAllPlanets function so it can be used by the planetsrouter
+// we're exporting the function like this because theyre are likely to be more functions to export. how things are exported effects how they need to be imported
+module.exports = {
+  httpGetAllPlanets,
+};
